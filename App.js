13b6ec843cd7e3dc47d9465a2d055a282819011a
= <=>
2023-11-14 17:18:54 +0300

    use zustand

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, StatusBar, Text, View, Pressable } from "react-native";
+import { useCounterStore, selectCount, selectIncrement } from "./store/counterStore";
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
+
+export default function App() {
+    const count = useCounterStore(selectCount);
+    const increment = useCounterStore(selectIncrement);
+
+    return (
+        <View style={styles.container}>
+            <StatusBar backgroundColor="white" barStyle="dark-content" />
+            <Text>Count: {count}</Text>
+            <Pressable onPress={() => increment(2323)}>
+                <Text>Click</Text>
+            </Pressable>
+        </View>
+    );
+}
